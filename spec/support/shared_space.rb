759ff3a3d97c08945538ae4d7b72b1f63706cc1d
Dmitriy Kalinin <pair+dmitriy@pivotallabs.com>
2013-07-05 19:05:43 -0700

    ignore already deleted space in shared space

--- spec/support/shared_space.rb
+++ spec/support/shared_space.rb
@@ -5,6 +5,10 @@ class SharedSpace
 
   def self.cleanup
     @space.delete!(:recursive => true) if @space
+  rescue CFoundry::SpaceNotFound => e
+    # space might have been deleted recursively
+    # when deleting organization
+    # in one of the after blocks
   end
 end
 
