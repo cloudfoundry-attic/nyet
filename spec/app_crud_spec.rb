d950462075ffe37ceeaf5a1c28e3fcfec65b0e1e
Dmitriy Kalinin and Edgar Honing <pair+dmitriy+edgar@pivotallabs.com>
2013-07-08 20:48:21 -0700

    only unindent

--- spec/app_crud_spec.rb
+++ spec/app_crud_spec.rb
@@ -17,37 +17,37 @@ describe "App CRUD" do
   with_health_monitoring "crud_app.health"
 
   it "creates/updates/deletes an app" do
-      monitoring.record_action(:create) do
-        app_name = "crud"
+    monitoring.record_action(:create) do
+      app_name = "crud"
 
-        regular_user.clean_up_app_from_previous_run(app_name)
-        @app = regular_user.create_app(@space, app_name)
+      regular_user.clean_up_app_from_previous_run(app_name)
+      @app = regular_user.create_app(@space, app_name)
 
-        regular_user.clean_up_route_from_previous_run(app_name)
-        @route = regular_user.create_route(@app)
-      end
+      regular_user.clean_up_route_from_previous_run(app_name)
+      @route = regular_user.create_route(@app)
+    end
 
-      monitoring.record_action(:push) do
-        deploy_app(@app)
-      end
+    monitoring.record_action(:push) do
+      deploy_app(@app)
+    end
 
-      monitoring.record_action(:start) do
-        AppWithStart.new(@app, APP_START_TIMEOUT, CHECK_DELAY).start_and_wait
-      end
+    monitoring.record_action(:start) do
+      AppWithStart.new(@app, APP_START_TIMEOUT, CHECK_DELAY).start_and_wait
+    end
 
-      monitoring.record_action(:app_routable) do
-        check_app_routable(@route)
-      end
+    monitoring.record_action(:app_routable) do
+      check_app_routable(@route)
+    end
 
-      monitoring.record_action(:update) do
-        scale_app(@app)
-      end
+    monitoring.record_action(:update) do
+      scale_app(@app)
+    end
 
-      monitoring.record_action(:delete) do
-        @route.delete!
-        @app.delete!
-        check_app_not_running(@route)
-      end
+    monitoring.record_action(:delete) do
+      @route.delete!
+      @app.delete!
+      check_app_not_running(@route)
+    end
   end
 
   def deploy_app(app)
