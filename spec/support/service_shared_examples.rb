ef9384e5b55732abab7c4e25441b2f0d3684884e
Dmitriy Kalinin and Edgar Honing <pair+dmitriy+edgar@pivotallabs.com>
2013-07-08 20:29:10 -0700

    refactor service shared example to user app_with_service

--- spec/support/service_shared_examples.rb
+++ spec/support/service_shared_examples.rb
@@ -1,69 +1,64 @@
-require "net/http"
-require "support/test_app"
+require "support/app_with_start"
+require "support/app_with_service"
 
 module ManageServiceHelpers
   def it_can_manage_service(options={})
-    app_name = options[:app_name] || raise("Missing app_name")
-    namespace = options[:namespace] || raise("Missing namespace")
-    plan_name = options[:plan_name] || raise("Missing plan_name")
+    app_name     = options[:app_name]     || raise("Missing app_name")
+    namespace    = options[:namespace]    || raise("Missing namespace")
+    plan_name    = options[:plan_name]    || raise("Missing plan_name")
     service_name = options[:service_name] || raise("Missing service_name")
 
     describe "manage service" do
       with_user_with_org
       with_shared_space
 
-      let(:dog_tags) { {service: app_name} }
-      let(:test_app_path) { "../../../apps/ruby/app_sinatra_service" }
+      with_health_monitoring "services.health"
+      with_tagged_monitoring service: app_name
 
-      it "allows users to create, bind, read, write, unbind, and delete the #{app_name} service" do
-        plan = regular_user.find_service_plan(service_name, plan_name)
-        plan.should be
+      before { regular_user.find_service_plan(service_name, plan_name) }
 
-        service_instance = nil
-        binding = nil
-        test_app = nil
+      before { @app = regular_user.create_app(space, app_name) }
+      after { @app.delete! if @app }
 
-        app = regular_user.create_app(space, app_name)
-        route = regular_user.create_route(app, "#{app_name}-#{SecureRandom.hex(2)}")
+      before { @route = regular_user.create_route(@app) }
+      after { @route.delete! if @route }
 
-        begin
-          monitoring.record_action("create_service", dog_tags) do
-            service_instance = regular_user.create_service_instance(space, service_name, plan_name)
-            service_instance.guid.should be
-          end
-
-          monitoring.record_action("bind_service", dog_tags) do
-            binding = regular_user.bind_service_to_app(service_instance, app)
-            binding.guid.should be
-          end
-
-          begin
-            app.upload(File.expand_path(test_app_path, __FILE__))
-            monitoring.record_action(:start, dog_tags) do
-              app.start!(true)
-              test_app = TestApp.new(app, route.name, service_instance, namespace)
-              test_app.when_running
-            end
-          rescue => e
-            raise if ENV["NYET_RAISE_ALL_ERRORS"]
-            pending "Unable to push an app. Possibly backend issue, error #{e.inspect}"
-          end
-
-          test_app.get_env
-
-          test_app.insert_value('key', 'value').should be_a Net::HTTPSuccess
-          test_app.get_value('key').should == 'value'
-          monitoring.record_metric("services.health", 1, dog_tags)
-        rescue RSpec::Core::Pending::PendingDeclaredInExample => e
-          raise e
-        rescue => e
-          monitoring.record_metric("services.health", 0, dog_tags)
-          raise e
+      it "creates/binds/reads/writes/unbinds/deletes a service" do
+        monitoring.record_action(:create_service) do
+          @service_instance = \
+            regular_user.create_service_instance(space, service_name, plan_name)
         end
 
-        binding.delete!
-        service_instance.delete!
-        app.delete!
+        monitoring.record_action(:bind_service) do
+          @service_binding = \
+            regular_user.bind_service_to_app(@service_instance, @app)
+        end
+
+        deploy_and_start_app(@app)
+
+        monitoring.record_action(:check_service) do
+          check_service_persists(@app, @service_instance, namespace)
+        end
+
+        monitoring.record_action(:delete_service) do
+          @service_binding.delete!
+          @service_instance.delete!
+        end
+      end
+
+      def deploy_and_start_app(app)
+        app.upload(File.expand_path("../../../apps/ruby/app_sinatra_service", __FILE__))
+        AppWithStart.new(app, 600, 2).start_and_wait
+      rescue => e
+        raise if ENV["NYET_RAISE_ALL_ERRORS"]
+        pending "Unable to push an app. Possibly backend issue, error #{e.inspect}"
+      end
+
+      def check_service_persists(app, service_instance, namespace)
+        aws = AppWithService.new(app, service_instance, namespace)
+        aws.get_env
+        aws.insert_value("key", "value")
+        aws.get_value("key").should == "value"
       end
     end
   end
