6b4f0ccedf11bdf304d2c471118203b7508fd08f
Alex Choi and Gregg Van Hove <pair+alex+gvanhove@pivotallabs.com>
2013-07-15 14:28:26 -0700

    add a 'full_run' monitoring entry for the full duration of the nyets

--- spec/app_crud_spec.rb
+++ spec/app_crud_spec.rb
@@ -15,36 +15,38 @@ describe "App CRUD" do
 
   it "creates/updates/deletes an app" do
     begin
-      monitoring.record_action(:create) do
-        app_name = "crud"
+      monitoring.record_action(:full_run) do
+        monitoring.record_action(:create) do
+          app_name = "crud"
 
-        regular_user.clean_up_app_from_previous_run(app_name)
-        @app = regular_user.create_app(@space, app_name)
+          regular_user.clean_up_app_from_previous_run(app_name)
+          @app = regular_user.create_app(@space, app_name)
 
-        regular_user.clean_up_route_from_previous_run(app_name)
-        @route = regular_user.create_route(@app, "#{app_name}-#{SecureRandom.uuid}")
-      end
+          regular_user.clean_up_route_from_previous_run(app_name)
+          @route = regular_user.create_route(@app, "#{app_name}-#{SecureRandom.uuid}")
+        end
 
-      monitoring.record_action(:read) do
-        deploy_app(@app)
-      end
+        monitoring.record_action(:read) do
+          deploy_app(@app)
+        end
 
-      monitoring.record_action(:start) do
-        start_app(@app)
-      end
+        monitoring.record_action(:start) do
+          start_app(@app)
+        end
 
-      monitoring.record_action(:app_routable) do
-        check_app_routable(@route)
-      end
+        monitoring.record_action(:app_routable) do
+          check_app_routable(@route)
+        end
 
-      monitoring.record_action(:update) do
-        scale_app(@app)
-      end
+        monitoring.record_action(:update) do
+          scale_app(@app)
+        end
 
-      monitoring.record_action(:delete) do
-        @route.delete!
-        @app.delete!
-        check_app_not_running(@route)
+        monitoring.record_action(:delete) do
+          @route.delete!
+          @app.delete!
+          check_app_not_running(@route)
+        end
       end
 
       monitoring.record_metric("crud_app.health", 1)
