16ce2638a18301c7d0133587a064eb74a3268c3e
Tony Hansmann <thansmann@rbcon.com>
2013-07-18 06:42:06 -0700

    give the router one second to register the url before testing it

--- spec/app_crud_spec.rb
+++ spec/app_crud_spec.rb
@@ -83,6 +83,7 @@ describe "App CRUD" do
   def check_app_routable(route)
     puts "starting #{__method__} (#{Time.now})"
     app_uri = URI("http://#{route.host}.#{route.domain.name}")
+    sleep 1
     content = Net::HTTP.get(app_uri)
     expect(content).to match(/^It just needed to be restarted!/)
   end
