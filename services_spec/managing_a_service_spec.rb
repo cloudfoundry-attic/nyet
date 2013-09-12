ed8808911528a9bfecfe747f1f40413fef9c7d81
Alex Choi and Matt Royal <pair+alex+mroyal@pivotallabs.com>
2013-09-12 10:39:30 -0700

    Revert "Purposely cause the managing a service test to fail to test alerting"
    This reverts commit 934361e26b6d6a581acb47a18bb7e4f04a50e46e.

--- services_spec/managing_a_service_spec.rb
+++ services_spec/managing_a_service_spec.rb
@@ -10,7 +10,6 @@ describe "Managing a Service", :appdirect => true, :cf => true do
 
   it "allows the user to push an app with a newly created service and bind it" do
     begin
-      raise "This is broken"
       Dir.chdir(test_app_path) do
         BlueShell::Runner.run("#{cf_bin} push --no-manifest --no-start") do |runner|
           runner.should say "Name>"
