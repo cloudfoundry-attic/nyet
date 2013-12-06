6b370a4fe1e738bc0c33f8c8a56a3aa905143a6e
Parker, Matt <mparker@pivotallabs.com>
2013-12-06 10:17:35 -0800

    Revert "mark the rediscloud isv spec as pending"
    This reverts commit 614a6b07328038ce5f872079d18563b92073d268.

--- services_spec/rediscloud_isv_spec.rb
+++ services_spec/rediscloud_isv_spec.rb
@@ -7,11 +7,9 @@ describe "Managing RedisCloud", :service => true, :appdirect => true do
   let(:service_name) { "rediscloud-dev" }
 
   it "allows users to create, bind, read, write, unbind, and delete the RedisCloud service" do
-    pending("app direct fixing their issues with redis cloud") do
-      create_and_use_managed_service do |client|
-        client.insert_value('key', 'value').should be_a Net::HTTPSuccess
-        client.get_value('key').should == 'value'
-      end
+    create_and_use_managed_service do |client|
+      client.insert_value('key', 'value').should be_a Net::HTTPSuccess
+      client.get_value('key').should == 'value'
     end
   end
 end
