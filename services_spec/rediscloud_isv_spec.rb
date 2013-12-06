c576ad9d5ed0ea383e8a3fcc447f21c2ace110a4
Parker, Matt <mparker@pivotallabs.com>
2013-12-06 10:21:04 -0800

    mark the rediscloud isv spec as pending
    Signed-off-by: alex choi <achoi@pivotallabs.com>

--- services_spec/rediscloud_isv_spec.rb
+++ services_spec/rediscloud_isv_spec.rb
@@ -7,9 +7,12 @@ describe "Managing RedisCloud", :service => true, :appdirect => true do
   let(:service_name) { "rediscloud-dev" }
 
   it "allows users to create, bind, read, write, unbind, and delete the RedisCloud service" do
-    create_and_use_managed_service do |client|
-      client.insert_value('key', 'value').should be_a Net::HTTPSuccess
-      client.get_value('key').should == 'value'
+    pending("app direct fixing their issues with redis cloud") do
+      create_and_use_managed_service do |client|
+        client.insert_value('key', 'value').should be_a Net::HTTPSuccess
+        client.get_value('key').should == 'value'
+      end
     end
+
   end
 end
