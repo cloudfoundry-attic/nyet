dea85cee2534e45a0f32f376e7d06573a0113878
Parker, Matt and alex choi <achoi@pivotallabs.com>
2013-12-17 16:03:59 -0800

    Marking sendgrid test as pending - they're having production issues
    Since we marked it pending inside the it block, the build will fail as
    soon as the test starts passing again.

--- services_spec/sendgrid_isv_spec.rb
+++ services_spec/sendgrid_isv_spec.rb
@@ -7,8 +7,10 @@ describe "Managing SendGrid", :service => true, :appdirect => true do
   let(:service_name) { "sendgrid-dev" }
 
   it "allows users to create, bind, send emails, unbind, and delete the SendGrid service" do
-    create_and_use_managed_service do |client|
-      client.send_email("john@example.com").should be_a Net::HTTPSuccess
+    pending "until sendgrid resolves their production issue causing random auth errors" do
+      create_and_use_managed_service do |client|
+        client.send_email("john@example.com").should be_a Net::HTTPSuccess
+      end
     end
   end
 end
