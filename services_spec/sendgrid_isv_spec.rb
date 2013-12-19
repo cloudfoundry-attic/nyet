f43d1920f23572399fb369a1edea8fa917c1a39f
Lucas Marks and Parker, Matt <lmarks@pivotallabs.com>
2013-12-19 15:39:58 -0800

    Removing pending test [Fixes #62674726]

--- services_spec/sendgrid_isv_spec.rb
+++ services_spec/sendgrid_isv_spec.rb
@@ -7,10 +7,8 @@ describe "Managing SendGrid", :service => true, :appdirect => true do
   let(:service_name) { "sendgrid-dev" }
 
   it "allows users to create, bind, send emails, unbind, and delete the SendGrid service" do
-    pending "until sendgrid resolves their production issue causing random auth errors" do
-      create_and_use_managed_service do |client|
-        client.send_email("john@example.com").should be_a Net::HTTPSuccess
-      end
+    create_and_use_managed_service do |client|
+      client.send_email("john@example.com").should be_a Net::HTTPSuccess
     end
   end
 end
