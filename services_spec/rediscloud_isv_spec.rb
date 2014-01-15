9b1b46751fdac909e49557e059c6bbb2c8392550
Christopher Jobst and alex choi <pair+cjobst+achoi@pivotallabs.com>
2014-01-15 11:20:07 -0800

    Revert "Rediscloud is failing due to them sending back an inappropriate response when an instance is already gone, waiting for them to change this."
    This reverts commit b58f4994e3aabf9f6543fddc1a41804aca34ee1c.

--- services_spec/rediscloud_isv_spec.rb
+++ services_spec/rediscloud_isv_spec.rb
@@ -6,8 +6,7 @@ describe "Managing RedisCloud", :service => true, :appdirect => true do
   let(:plan_name) { "25mb" }
   let(:service_name) { "rediscloud-dev" }
 
-  #TODO: make this not pending once rediscloud / appdirect fix this issue
-  xit "allows users to create, bind, read, write, unbind, and delete the RedisCloud service" do
+  it "allows users to create, bind, read, write, unbind, and delete the RedisCloud service" do
     create_and_use_managed_service do |client|
       client.insert_value('key', 'value').should be_a Net::HTTPSuccess
       client.get_value('key').should == 'value'
